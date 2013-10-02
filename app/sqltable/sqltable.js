507b0713d5f7f6a8d7187273846a9aac20198317
Beyang Liu <beyang.liu@gmail.com>
2013-10-02 16:28:13 -0700

    expandGroup function and query as attribute

--- app/sqltable/sqltable.js
+++ app/sqltable/sqltable.js
@@ -11,13 +11,20 @@ angular.module('sqltable', [
   return {
     restrict: 'E',
     replace: true,
-    scope: {},
+    scope: {
+      query: "@query",
+    },
     templateUrl: '/sqltable/sqltable.tpl.html',
     controller: function($scope, sqltableServer) {
+      $scope.queryInProgress = $scope.query;
+
       $scope.updateQuery = function($event) {
         $scope.query = $scope.queryInProgress;
         $scope.data = sqltableServer.get({'query':$scope.query});
       };
+      $scope.expandGroup = function(row, col) {
+        console.log(row, col);
+      }
       $scope.data = {};
     },
   };
