880a30302f4f7ed656cdc5bf3bae83833fa22e1c
Beyang Liu <beyang.liu@gmail.com>
2013-10-01 21:20:06 -0700

    sql query controls

--- src/sqltable.js
+++ src/sqltable.js
@@ -8,11 +8,31 @@ angular.module('sqltable', [
     replace: true,
     scope: {},
     templateUrl: '/src/sqltable.tpl.html',
-    link: function(scope, elem, attr) {
-      // TODO
-    },
+    // link: function(scope, elem, attr) {
+    //   // TODO
+    // },
     controller: function($scope) {
-      // TODO
+      $scope.select = '';
+      $scope.from = '';
+      $scope.where = '';
+      $scope.groupBy = '';
+      $scope.orderBy = '';
+
+      var syncQuery = function() {
+        var select = $scope.select;
+        var from = $scope.from;
+        var where = $scope.where;
+        var groupBy = $scope.groupBy;
+        var orderBy = $scope.orderBy;
+        $scope.query = 'SELECT '+$scope.select+' FROM '+$scope.from+' WHERE '+$scope.where+' GROUP BY '+$scope.groupBy+' ORDER BY '+$scope.orderBy;
+      };
+
+      var clauses = ['select', 'from', 'where', 'groupBy', 'orderBy'];
+      clauses.forEach(function(clause) {
+        $scope.$watch(clause, function() {
+          syncQuery()
+        });
+      });
     }
   };
 })
