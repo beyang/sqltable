bf21661dbf123685e456175da332b372914db6d4
Beyang Liu <beyang.liu@gmail.com>
2013-10-01 18:40:14 -0700

    initial files

---
+++ example/index.js
@@ -0,0 +1,5 @@
+angular.module('sqltable-example', [
+  'sqltable',
+])
+
+;
---
+++ src/sqltable.js
@@ -0,0 +1,20 @@
+angular.module('sqltable', [
+
+])
+
+.directive('sqltable', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {},
+    templateUrl: '/src/sqltable.tpl.html',
+    link: function(scope, elem, attr) {
+      // TODO
+    },
+    controller: function($scope) {
+      // TODO
+    }
+  };
+})
+
+;
