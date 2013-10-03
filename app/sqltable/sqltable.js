0c99dff3e3643a5941356b330b2bf147b9aace35
Beyang Liu <beyang.liu@gmail.com>
2013-10-02 22:14:07 -0700

    expandable rows

--- app/sqltable/sqltable.js
+++ app/sqltable/sqltable.js
@@ -30,6 +30,25 @@ angular.module('sqltable', [
   };
 })
 
+.directive('sqltableWrapper', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      query: "@query",
+    },
+    template: "<div ng-include=\"'sqltable/sqltableInclude.html'\"></div>",
+    controller: function($scope) {
+      var query = JSON.parse($scope.query);
+      $scope.select = query.select.join('\n');
+      $scope.from = query.from.join('\n');
+      $scope.where = query.where.join('\n');
+      $scope.orderBy = query.orderBy.join('\n');
+      $scope.groupBy = query.groupBy.join('\n');
+    },
+  };
+})
+
 .directive('sqltable', function() {
   return {
     restrict: 'E',
@@ -43,7 +62,10 @@ angular.module('sqltable', [
     },
     templateUrl: '/sqltable/sqltable.tpl.html',
     controller: function($scope, sqltableServer, queryToString) {
+      console.log('initing table');
+
       // Wiring
+      $scope.rowSubQueries = {};
       $scope.queryString = function() {
         return queryToString($scope.query);
       }
@@ -60,7 +82,13 @@ angular.module('sqltable', [
         $scope.data = sqltableServer.get({'query':$scope.queryString()});
       };
 
-      $scope.expandGroup = function(row, col) {
+      $scope.toggleExpandRow = function(row, col) {
+        if ($scope.rowSubQueries[row]) {
+          $scope.rowSubQueries[row] = undefined;
+          return;
+        }
+
+        // Maybe derive drilldown query and expand
         var colName = $scope.data.columnNames[col];
         var rawVal = $scope.data.rows[row][col];
         var valClause;
@@ -72,29 +100,28 @@ angular.module('sqltable', [
           valClause = "="+rawVal.toString();
         }
 
-        var drillQuery = {
-          'select': undefined,  // initialized later on
-          'from': $scope.query.from.slice(0),
-          'where': $scope.query.where.slice(0),
-          'groupBy': $scope.query.groupBy.slice(0),
-          'orderBy': $scope.query.orderBy.slice(0),
-        };
-
         var gIndex = col >= $scope.query.groupBy.length ? -1 : col;
         if (gIndex >= 0) {
+          var drillQuery = {
+            'select': undefined,  // initialized later on
+            'from': $scope.query.from.slice(0),
+            'where': $scope.query.where.slice(0),
+            'groupBy': $scope.query.groupBy.slice(0),
+            'orderBy': $scope.query.orderBy.slice(0),
+          };
+
           var drillGroup = drillQuery['groupBy'].splice(gIndex, 1); // (note: drillGroup is often, but not always equal to colName)
           drillQuery['where'].push(drillGroup+valClause);
 
           if (drillQuery['groupBy'].length === 0) {
             drillQuery['select'] = ['*'];
           } else {
-            drillQuery['select'] = drillQuery['groupBy'].slice(0);
-            drillQuery['select'].push('count(*)');
+            drillQuery['select'] = ['count(*)'];
           }
 
-          console.log(queryToString(drillQuery));
+          // console.log(queryToString(drillQuery));
+          $scope.rowSubQueries[row] = drillQuery;
 
-          // TODO: add new table
         } else {
           console.log('nothing to drill');
         }
